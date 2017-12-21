8c6eeeda02c60a08ef128179d783361f72368dd2
rasmusbihllarsen <rasmus@rblarsen.dk>
2017-12-21 13:45:20 +0100

    Fixed warning because of wron inclusion of spinner

--- transitions/fade.php
+++ transitions/fade.php
@@ -1,7 +1,7 @@
 <div class="rbl_fake_transitions--fade active" style="background-color:<?php echo $transition_main_color ?>;">
 	<?php
 		if(!empty($spinner_type)){
-			include('../spinners/'.$spinner_type.'.php');
+			include(plugin_dir_path(__FILE__).'/../spinners/'.$spinner_type.'.php');
 		}
 	?>
 </div>
